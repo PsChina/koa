f16c1e3eecabb19f15d2ef99c6b7eccc2c2aa1d8
PsChina <panshanzxcv.love@163.com>
2018-08-15 07:44:01 +0800

    测试string类型的body

---
+++ 基本用法/demos05.js
@@ -0,0 +1,22 @@
+// 测试 string 类型的body
+const Koa = require('koa'); // 获取koa
+const app = new Koa(); // 新建实例
+const main = ctx => {
+    ctx.response.type = 'html'; //  设置响应类型
+    ctx.response.body = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1>Hello World! (type: string)</h1>
+    </body>
+    </html>
+    `
+}
+app.use(main);
+app.listen(3000) // 监听3000端口
